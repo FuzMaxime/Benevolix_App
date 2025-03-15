1cde627f92528806db5ba018c6682c5e1c8e0dd3
evan <evan.ferron53@gmail.com>
2025-03-15 10:12:53 +0100

    :sparkles: | create login page

---
+++ benevolix_app/lib/widgets/headerAuth.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../constants/color.dart';
+
+class HeaderAuth extends StatelessWidget {
+  final String title;
+
+  const HeaderAuth({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 60.0,
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Image.asset('Benevolix.png'),
+        ),
+        Column(
+          spacing: 25.0,
+          children: [
+            Align(
+                child: Title(
+                    color: ColorConstant.red,
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        color: ColorConstant.red,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ))),
+            Text(
+              "Ti brudet prenañ doñv c’hoari poull mat paeañ Brasparz kribañ e daouarn  goleiñ zo",
+              style: TextStyle(
+                  color: ColorConstant.grey, fontWeight: FontWeight.w400),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
