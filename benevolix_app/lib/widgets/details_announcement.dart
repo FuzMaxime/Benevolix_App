1f9995f885fcbb69807682703d96655bf077bab5
fmaxime <maxime.fuzeau@ynov.com>
2025-03-31 09:54:01 +0200

    :lipstick: | improve UI of detailAnnouncement widget

--- benevolix_app/lib/widgets/details_announcement.dart
+++ benevolix_app/lib/widgets/details_announcement.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:benevolix_app/widgets/submit_button.dart';
 import 'package:flutter/material.dart';
 import 'package:benevolix_app/services/candidature_service.dart';
+import 'package:benevolix_app/widgets/profile_picture.dart';
 
 class DetailsAnnouncement extends StatefulWidget {
   final Announcement announcement;
@@ -25,7 +26,6 @@ class _DetailsAnnouncementState extends State<DetailsAnnouncement> {
     announcement = widget.announcement;
   }
 
-  @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.8,
@@ -36,24 +36,24 @@ class _DetailsAnnouncementState extends State<DetailsAnnouncement> {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  backgroundColor: ColorConstant.red,
-                  child: Text(
-                    "JD",
-                    //announcement.title.substring(0, 2).toUpperCase(),
-                    style: TextStyle(color: ColorConstant.white),
+            Container(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ProfilePicture(
+                    firstName: announcement.title,
+                    lastName: announcement.title,
+                    size: AvatarSize.small,
                   ),
-                ),
-                IconButton(
-                  icon: Image.asset("assets/images/close.png"),
-                  onPressed: () {
-                    Navigator.pop(context); // Ferme la modal
-                  },
-                ),
-              ],
+                  IconButton(
+                    icon: Image.asset("assets/images/close.png"),
+                    onPressed: () {
+                      Navigator.pop(context); // Ferme la modal
+                    },
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 15),
             Text(
@@ -66,42 +66,57 @@ class _DetailsAnnouncementState extends State<DetailsAnnouncement> {
               ),
             ),
             SizedBox(height: 15),
-            Row(
-              children: [
-                Icon(Icons.location_on, color: ColorConstant.grey),
-                SizedBox(width: 4),
-                Expanded(
-                  child: Text(
-                    announcement.adress,
-                    style: TextStyle(color: ColorConstant.grey),
-                    overflow: TextOverflow.visible, // Permet de rendre le texte visible en cas de retour à la ligne
-                    softWrap: true, // Active le retour à la ligne automatique
+            Container(
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Icon(Icons.location_on, color: ColorConstant.grey),
+                  SizedBox(width: 4),
+                  Expanded(
+                    child: Text(
+                      announcement.adress,
+                      style: TextStyle(color: ColorConstant.grey),
+                      overflow: TextOverflow.visible,
+                      softWrap: true,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             SizedBox(height: 15),
-            Row(
-              children: [
-                Icon(Icons.calendar_today, color: ColorConstant.grey),
-                SizedBox(width: 4),
-                Text(
-                  DateFormat('dd/MM/yyyy').format(DateTime.parse(announcement.date)),
-                  style: TextStyle(color: ColorConstant.grey),
-                ),
-                SizedBox(width: 16),
-                Icon(Icons.timer, color: ColorConstant.grey),
-                SizedBox(width: 4),
-                Text("${announcement.duration} heures", style: TextStyle(color: ColorConstant.grey)),
-                SizedBox(width: 16),
-                Icon(announcement.isRemote ? Icons.computer : Icons.business, color: ColorConstant.grey),
-              ],
+            Container(
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Icon(Icons.calendar_today, color: ColorConstant.grey),
+                  SizedBox(width: 4),
+                  Text(
+                    DateFormat('dd/MM/yyyy').format(DateTime.parse(announcement.date)),
+                    style: TextStyle(color: ColorConstant.grey),
+                  ),
+                  SizedBox(width: 16),
+                  Icon(Icons.access_time, color: ColorConstant.grey),
+                  SizedBox(width: 4),
+                  Text(
+                    "${announcement.duration} heures",
+                    style: TextStyle(color: ColorConstant.grey),
+                  ),
+                  SizedBox(width: 16),
+                  Icon(
+                    announcement.isRemote ? Icons.computer : Icons.business,
+                    color: ColorConstant.grey,
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 15),
             Wrap(
               spacing: 8,
               children: announcement.tags.map((tag) => Chip(
-                label: Text(tag.name, style: TextStyle(color: ColorConstant.red)),
+                label: Text(
+                  tag.name,
+                  style: TextStyle(color: ColorConstant.red),
+                ),
                 backgroundColor: ColorConstant.darkRed,
               )).toList(),
             ),
@@ -116,6 +131,7 @@ class _DetailsAnnouncementState extends State<DetailsAnnouncement> {
             ),
             SizedBox(height: 15),
             Container(
+              width: double.infinity,
               padding: const EdgeInsets.all(12),
               decoration: BoxDecoration(
                 color: ColorConstant.darkGrey,
@@ -128,16 +144,20 @@ class _DetailsAnnouncementState extends State<DetailsAnnouncement> {
               ),
             ),
             const SizedBox(height: 16),
-            SubmitButton(
-              text: 'Je souhaite participer',
-              onPressed: () {
-                createCandidature(announcement.id, DateTime.now(), "Waiting", 2);
-              },
-              isLoading: isLoading,
+            SizedBox(
+              width: double.infinity,
+              child: SubmitButton(
+                text: 'Je souhaite participer',
+                onPressed: () {
+                  createCandidature(announcement.id, DateTime.now(), "Waiting", announcement.ownerId);
+                },
+                isLoading: isLoading,
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
 }
